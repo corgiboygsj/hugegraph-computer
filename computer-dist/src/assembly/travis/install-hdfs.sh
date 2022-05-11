0cf18825e58ca79cbb73c307d76910bc46b6e710
ShouJing <1075185785@qq.com>
2022-05-11 11:16:31 +0800

    fix ci install hdfs

--- computer-dist/src/assembly/travis/install-hdfs.sh
+++ computer-dist/src/assembly/travis/install-hdfs.sh
@@ -2,54 +2,53 @@
 
 set -ev
 
-UBUNTU_VERSION=$(lsb_release -r | awk '{print substr($2,0,2)}')
+TRAVIS_DIR=`dirname $0`
+HDFS_VERSION=$1
+HDFS_URL="https://dlcdn.apache.org/hadoop/common/hadoop-${HDFS_VERSION}/hadoop-${HDFS_VERSION}.tar.gz"
 
-tee /etc/apt/sources.list.d/hdp.list <<EOF
-deb http://public-repo-1.hortonworks.com/HDP/ubuntu${UBUNTU_VERSION}/2.x/updates/2.6.5.0 HDP main
-EOF
+cd ${TRAVIS_DIR}
+wget -O hdfs.tar.gz ${HDFS_URL}
+mkdir hdfs
+tar -zxvf hdfs.tar.gz -C hdfs --strip-components 1
 
-apt-get update
+HDFS_HOME=${TRAVIS_DIR}/hdfs
 
-mkdir -p /etc/hadoop/conf
-tee /etc/hadoop/conf/core-site.xml <<EOF
+tee ${HDFS_HOME}/etc/hadoop/conf/core-site.xml <<EOF
 <configuration>
     <property>
         <name>fs.defaultFS</name>
-        <value>hdfs://localhost:8020</value>
+        <value>hdfs://localhost:9000</value>
     </property>
 </configuration>
 EOF
 
-tee /etc/hadoop/conf/hdfs-site.xml <<EOF
+tee ${HDFS_HOME}/etc/hadoop/hdfs-site.xml <<EOF
 <configuration>
     <property>
-        <name>dfs.namenode.name.dir</name>
-        <value>/opt/hdfs/name</value>
+        <name>dfs.replication</name>
+        <value>1</value>
     </property>
     <property>
-        <name>dfs.datanode.data.dir</name>
-        <value>/opt/hdfs/data</value>
-    </property>
-    <property>
-        <name>dfs.permissions.superusergroup</name>
-        <value>hadoop</value>
-    </property>
-    <property>
-        <name>dfs.support.append</name>
-        <value>true</value>
+        <name>dfs.secondary.http.address</name>
+        <value>localhost:9100</value>
     </property>
 </configuration>
 EOF
 
-apt-get install -y --allow-unauthenticated hadoop hadoop-hdfs
+chmod g-w $HOME                                 &&
+chmod o-w $HOME                                 &&
+ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa        &&
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys &&
+chmod 0600 ~/.ssh/authorized_keys               &&
+ssh-keyscan -H localhost >> ~/.ssh/known_hosts  &&
+chmod 0600 ~/.ssh/known_hosts                   &&
+eval \`ssh-agent\`                              &&
+ssh-add ~/.ssh/id_rsa
 
-mkdir -p /opt/hdfs/data /opt/hdfs/name
-chown -R hdfs:hdfs /opt/hdfs
--u hdfs hdfs namenode -format -nonInteractive
+export HADOOP_HOME=${HDFS_HOME}
+export HDFS_NAMENODE_ADDR=127.0.0.1:9000
+export PATH=$HADOOP_HOME/bin:$PATH
+source /etc/profile
 
-adduser travis hadoop
-
-/usr/hdp/current/hadoop-hdfs-datanode/../hadoop/sbin/hadoop-daemon.sh start datanode
-/usr/hdp/current/hadoop-hdfs-namenode/../hadoop/sbin/hadoop-daemon.sh start namenode
-
-hdfs dfsadmin -safemode wait
+${HDFS_HOME}/bin/hdfs namenode -format
+${HDFS_HOME}/sbin/start-dfs.sh
